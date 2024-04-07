31371fae981c795b9a9b51dda54d05ae2d1847e0
kimHyeonJu <kimhyesom1202@gmail.com>
2024-04-07 11:38:59 +0900

    footer_menu 수정

--- src/pages/Homepage/components/Footer/Footer.jsx
+++ src/pages/Homepage/components/Footer/Footer.jsx
@@ -17,21 +17,20 @@ const Footer = () => {
       <div className={`${style.footer_menu}`}>
         <ul className={`${style.footer_menu_list}`}>
           <li className={`${style.footer_menu_item}`}>자주 묻는 질문</li>
-          <li className={`${style.footer_menu_item}`}>투자 정보(IR)</li>
           <li className={`${style.footer_menu_item}`}>개인정보 처리방침</li>
           <li className={`${style.footer_menu_item}`}>속도 테스트</li>
         </ul>
         <ul className={`${style.footer_menu_list}`}>
           <li className={`${style.footer_menu_item}`}>고객 센터</li>
-          <li className={`${style.footer_menu_item}`}>입사 정보</li>
+          <li className={`${style.footer_menu_item}`}>깃허브 정보</li>
           <li className={`${style.footer_menu_item}`}>쿠키 설정</li>
           <li className={`${style.footer_menu_item}`}>법적 고지</li>
         </ul>
         <ul className={`${style.footer_menu_list}`}>
           <li className={`${style.footer_menu_item}`}>계정</li>
-          <li className={`${style.footer_menu_item}`}>넷플릭스 지원 디바이스</li>
-          <li className={`${style.footer_menu_item}`}>회사 정보</li>
-          <li className={`${style.footer_menu_item}`}>오직 넷플릭스에서</li>
+          <li className={`${style.footer_menu_item}`}>현플릭스 지원 디바이스</li>
+          <li className={`${style.footer_menu_item}`}>개인 정보</li>
+          <li className={`${style.footer_menu_item}`}>오직 현플릭스에서</li>
         </ul>
         <ul className={`${style.footer_menu_list}`}>
           <li className={`${style.footer_menu_item}`}>미디어 센터</li>
@@ -41,7 +40,7 @@ const Footer = () => {
       </div>
       <div className={`${style.copyright}`}>
         <p>© Copyright 2024-04-06~ (KimHyeonJu)</p>
-        <p>https://github.com/hyesom2/5_Netflix_Demo</p>
+        <p>https://github.com/hyesom2/5_HyeonFlix_Demo</p>
         <p>상업적 용도가 아닌 개인 프로젝트 입니다.</p>
       </div>
     </div>
