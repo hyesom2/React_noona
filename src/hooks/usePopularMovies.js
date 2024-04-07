b25db7f5620b0d9e75bbb24834b50037d9746f34
kimHyeonJu <kimhyesom1202@gmail.com>
2024-04-07 18:30:45 +0900

    ✅ 여러가지 api 시도

--- src/hooks/usePopularMovies.js
+++ src/hooks/usePopularMovies.js
@@ -2,12 +2,17 @@ import { useQuery } from '@tanstack/react-query';
 import api from '../utils/api';
 
 const fetchPopularMovies = () => {
+  // return  api.get(`/genre/movie/list`);
   return  api.get(`/movie/popular`);
+  // return  api.get(`/movie/popular?include_adult=true&include_video=true`);
   // return  api.get(`/movie/popular?language=ko-KR`);
+  // return api.get(`/movie/popular?include_adult&include_video=true&sort_by=popularity.desc`);
+  // return api.get(`/discover/movie?include_adult=true&include_video=true&page=1&sort_by=vote_average.desc`);
 };
 
 export const usePopularMoviesQuery = () => {
   return useQuery({
+    method: 'GET',
     queryKey: ['movie-popular'],
     queryFn: fetchPopularMovies,
     select: (result) => result.data,
