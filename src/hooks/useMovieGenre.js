cead985107561ec5a74343bfe4b9fe6f8deb3910
kimHyeonJu <kimhyesom1202@gmail.com>
2024-04-09 00:41:08 +0900

    ✨ useMovieGenreQuery 생성 및 MovieCard 에 적용

---
+++ src/hooks/useMovieGenre.js
@@ -0,0 +1,16 @@
+import { useQuery } from '@tanstack/react-query';
+import api from '../utils/api';
+
+const fetchMovieGenre = () => {
+  return api.get(`/genre/movie/list`);
+};
+
+export const useMovieGenreQuery = () => {
+  return useQuery({
+    method: 'GET',
+    queryKey: ['movie-genre'],
+    queryFn: fetchMovieGenre,
+    select: (result) => result.data.genres,
+    staleTime: 300000 // 호출되는 시간을 지정
+  });
+};
--- src/pages/Homepage/components/MovieCard/MovieCard.jsx
+++ src/pages/Homepage/components/MovieCard/MovieCard.jsx
@@ -6,9 +6,19 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEllipsis } from '@fortawesome/free-solid-svg-icons';
 // > react-router
 import { Link } from 'react-router-dom';
+import { useMovieGenreQuery } from '../../../../hooks/useMovieGenre';
 
 const MovieCard = ({ movie }) => {
   const img_base_url = `https://media.themoviedb.org/t/p/w300_and_h450_bestv2/`;
+  const { data:genreData } = useMovieGenreQuery(); // data:genreData는 data이름을 genreData로 재정의한다는 뜻
+  const showGenre = (genreIdList) => {
+    if( !genreData ){ return [] };
+    const genreNameList = genreIdList.map((id) => {
+      const genreObj = genreData.find((genre) => genre.id === id);
+      return genreObj.name;
+    });
+    return genreNameList;
+  };
 
   return (
     <Link to={`/${movie?.id}`}>
@@ -16,6 +26,13 @@ const MovieCard = ({ movie }) => {
         <img src={ `${img_base_url}` + movie?.poster_path } alt={ movie?.title } />
         <div className="movie_card_info">
           <h3 className="movie_title">{ movie?.title }</h3>
+          <ul className="movie_genre_list">
+            {
+              showGenre(movie?.genre_ids).map((genre, index) => (
+                <li className="movie_genre_item" key={ index }>{ genre }</li>
+              ))
+            }
+          </ul>
           <p className="movie_release_date">{ movie?.release_date }</p>
           <FontAwesomeIcon icon={faEllipsis} className="icon-more" />     
         </div>
