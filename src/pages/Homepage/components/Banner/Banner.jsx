b679603593283fe06c758af48168bd690da304e2
kimHyeonJu <kimhyesom1202@gmail.com>
2024-04-09 16:34:21 +0900

    ♻️ banner_info_title 수정, 모바일버전 banner background의 url 수정

--- src/pages/Homepage/components/Banner/Banner.jsx
+++ src/pages/Homepage/components/Banner/Banner.jsx
@@ -5,7 +5,6 @@ import { Desktop, Mobile } from '../../../../hooks/responsive';
 
 const Banner = () => {
   const { data, isLoading, isError, error } = usePopularMoviesQuery();
-
   if(isLoading){
     <h1>Loading...</h1>
   }
@@ -17,38 +16,32 @@ const Banner = () => {
 
   return (
     <>
-    <Desktop>
-      <div className="banner_background" style={{ backgroundImage: `url(${banner_background_url})`}}>
-        <div className="banner_info">
-          <h1 className="banner_info_title">{ data?.results[1].original_title }</h1>
-          <ul className="banner_info_list">
-            <li>{ data?.results[1].release_date }</li>
-            <li>{ data?.results[1].vote_average }</li>
-            <li>{ data?.results[1].original_language }</li>
-          </ul>
-          <p className="banner_info_desc">{ data?.results[1].overview}</p>
+      <Desktop>
+        <div className="banner_background" style={{ backgroundImage: `url(${banner_background_url})`}}>
+          <div className="banner_info">
+            <h1 className="banner_info_title">{ data?.results[1].title }</h1>
+            <ul className="banner_info_list">
+              <li>{ data?.results[1].release_date }</li>
+              <li>{ data?.results[1].vote_average }</li>
+              <li>{ data?.results[1].original_language }</li>
+            </ul>
+            <p className="banner_info_desc">{ data?.results[1].overview}</p>
+          </div>
         </div>
-      </div>
-    </Desktop>
-    <Mobile>
-      <div
-          className="banner_background_mobile" 
-          style={{ backgroundImage: 
-            "url("
-            +`https://media.themoviedb.org/t/p/w1920_and_h800_multi_faces/${data?.results[1].backdrop_path}` 
-            + ")",
-          }}>
-        <div className="banner_info_mobile">
-          <h1 className="banner_info_title_mobile">{ data?.results[1].original_title }</h1>
-          <ul className="banner_info_list_mobile">
-            <li>{ data?.results[1].release_date }</li>
-            <li>{ data?.results[1].vote_average }</li>
-            <li>{ data?.results[1].original_language }</li>
-          </ul>
-          <p className="banner_info_desc_mobile">{ data?.results[1].overview}</p>
+      </Desktop>
+      <Mobile>
+        <div className="banner_background_mobile" style={{ backgroundImage: `url(${banner_background_url})`}}>
+          <div className="banner_info_mobile">
+            <h1 className="banner_info_title_mobile">{ data?.results[1].title }</h1>
+            <ul className="banner_info_list_mobile">
+              <li>{ data?.results[1].release_date }</li>
+              <li>{ data?.results[1].vote_average }</li>
+              <li>{ data?.results[1].original_language }</li>
+            </ul>
+            <p className="banner_info_desc_mobile">{ data?.results[1].overview}</p>
+          </div>
         </div>
-      </div>
-    </Mobile>
+      </Mobile>
     </>
   )
 }
