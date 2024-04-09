b1466d4b067d33aa8acba658fc423215bdf16cc5
kimHyeonJu <kimhyesom1202@gmail.com>
2024-04-09 16:47:35 +0900

    ✨ TrendingMovieSlide component 구현

---
+++ src/hooks/useTrendingMovies.js
@@ -0,0 +1,15 @@
+import { useQuery } from '@tanstack/react-query';
+import api from '../utils/api';
+
+const fetchTrendingMovies = () => {
+  return api.get(`/trending/movie/day?watch_region=KR&language=ko`);
+};
+
+export const useTrendingMovieQuery = () => {
+  return useQuery({
+    method: 'GET',
+    queryKey: ['upcoming-movie'],
+    queryFn: fetchTrendingMovies,
+    select: (result) => result.data
+  });
+};
