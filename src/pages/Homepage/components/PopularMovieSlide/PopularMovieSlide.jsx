c2193cde85445b4331a0e797fadc29b52f5cea8f
kimHyeonJu <kimhyesom1202@gmail.com>
2024-04-13 21:04:15 +0900

    🔥 console.log 삭제

--- src/pages/Homepage/components/PopularMovieSlide/PopularMovieSlide.jsx
+++ src/pages/Homepage/components/PopularMovieSlide/PopularMovieSlide.jsx
@@ -9,7 +9,6 @@ import { responsive } from '../../../../constants/SliderResponsive';
 
 const PopularMovieSlide = () => {
   const { data, isLoading, isError, error } = usePopularMoviesQuery();
-  console.log("popular Data", data);
   if(isLoading){
     return <h1>Loading...</h1>
   }
