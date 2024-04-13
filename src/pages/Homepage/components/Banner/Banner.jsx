0033c28be900d1eb09cd83bd30018df5e47ce0d1
kimHyeonJu <kimhyesom1202@gmail.com>
2024-04-13 21:46:06 +0900

    ♻️ styled-components로 변경 및  모바일 반응형 작업

--- src/pages/Homepage/components/Banner/Banner.jsx
+++ src/pages/Homepage/components/Banner/Banner.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 // > css (styled-components)
 import * as s from './Banner.style.js';
+// > hook
 import { usePopularMoviesQuery } from '../../../../hooks/usePopularMovies';
-import { Desktop, Mobile } from '../../../../hooks/responsive';
 
 const Banner = () => {
   const { data, isLoading, isError, error } = usePopularMoviesQuery();
@@ -16,34 +16,25 @@ const Banner = () => {
   const banner_background_url = `https://media.themoviedb.org/t/p/w1920_and_h800_multi_faces/${data?.results[1].poster_path}`;
 
   return (
-    <>
-      <Desktop>
-        <s.Banner style={{ backgroundImage: `url(${banner_background_url})`}}>
-          <s.Banner_Info>
-            <h1 className="banner_info_title">{ data?.results[1].title }</h1>
-            <ul className="banner_info_list">
-              <li className="banner_info_item">{ data?.results[1].release_date }</li>
-              <li className="banner_info_item">{ data?.results[1].vote_average }</li>
-              <li className="banner_info_item">{ data?.results[1].original_language }</li>
-            </ul>
-            <p className="banner_info_desc">{ data?.results[1].overview}</p>
-          </s.Banner_Info>
-        </s.Banner>
-      </Desktop>
-      <Mobile>
-        <div className="banner_background_mobile" style={{ backgroundImage: `url(${banner_background_url})`}}>
-          <div className="banner_info_mobile">
-            <h1 className="banner_info_title_mobile">{ data?.results[1].title }</h1>
-            <ul className="banner_info_list_mobile">
-              <li>{ data?.results[1].release_date }</li>
-              <li>{ data?.results[1].vote_average }</li>
-              <li>{ data?.results[1].original_language }</li>
-            </ul>
-            <p className="banner_info_desc_mobile">{ data?.results[1].overview}</p>
-          </div>
-        </div>
-      </Mobile>
-    </>
+    <s.Container style={{ backgroundImage: `url(${banner_background_url})` }}>
+      <s.Banner_Info>
+        <h1 className="banner_info_title">{ data?.results[1].title }</h1>
+        <ul className="banner_info_list">
+          <li className="banner_info_item">{ data?.results[1].release_date }</li>
+          <li className="banner_info_item">{ data?.results[1].vote_average }</li>
+          <li className="banner_info_item">{ data?.results[1].original_language }</li>
+        </ul>
+        <p className="banner_info_desc">
+          {
+            data?.results[1].overview.length >= 200
+            ?
+            data?.results[1].overview.slice(0, 199) + "..."
+            :
+            data?.results[1].overview
+          }
+        </p>
+      </s.Banner_Info>
+    </s.Container>
   )
 }
 
--- src/pages/Homepage/components/Banner/Banner.style.js
+++ src/pages/Homepage/components/Banner/Banner.style.js
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 
-export const Banner = styled.div`
+export const Container = styled.div`
   position: relative;
+  width: 100%;
   height: 600px;
   background-repeat: no-repeat;
   background-position: center center;
@@ -16,12 +17,29 @@ export const Banner = styled.div`
     background: linear-gradient(to bottom, transparent, #141414);
     z-index: 99;
   }
+
+  @media screen and (max-width: 1023px) {
+    height: 500px;
+
+    &::before {
+      height: 500px;
+    }
+  }
+
+  @media screen and (max-width: 767px) {
+    height: 400px;
+
+    &::before {
+      height: 400px;
+    }
+  }
 `;
 
 export const Banner_Info = styled.div`
   position: absolute;
-  top: 200px;
+  top: 50%;
   left: 60px;
+  transform: translateY(-50%);
   width: 500px;
   z-index: 100;
 
@@ -57,59 +75,26 @@ export const Banner_Info = styled.div`
   .banner_info_desc {
     font-size: 16px;
   }
-`;
-// /* mobile */
-// .banner_background_mobile {
-//   position: relative;
-//   height: 500px;
-//   background-repeat: no-repeat;
-//   background-position: center center;
-//   background-size: cover;
-// }
-// .banner_background_mobile::before {
-//   content: '';
-//   position: absolute;
-//   left: 0;
-//   width: 100%;
-//   height: 500px;
-//   background: linear-gradient(to bottom, transparent, #141414);
-//   z-index: 99;
-// }
 
-// .banner_info_mobile {
-//   position: absolute;
-//   top: 50%;
-//   left: 50%;
-//   transform: translate(-50%, -50%);
-//   width: 300px;
-//   z-index: 100;
-// }
-// .banner_info_mobile h1 {
-//   font-size: 35px;
-//   font-weight: bold;
-//   text-align: center;
-//   margin-bottom: 15px;
-// }
-// .banner_info_list_mobile {
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-//   margin-bottom: 15px;
-// }
-// .banner_info_list_mobile li {
-//   font-size: 14px;
-//   color: #E5E5E5;
-// }
-// .banner_info_list_mobile li:after {
-//   content: '|';
-//   margin: 0 10px;
-// }
-// .banner_info_list_mobile li:last-child {
-//   margin-right: 0;
-// }
-// .banner_info_list_mobile li:last-child::after {
-//   content: '';
-// }
-// .banner_info_mobile .desc {
-//   font-size: 16px;
-// }
+  @media screen and (max-width: 767px) {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 300px;
+
+    .banner_info_title {
+      font-size: 35px;
+      margin-bottom: 10px;
+    }
+
+    .banner_info_list {
+      display: flex;
+      margin-bottom: 10px;
+    }
+
+    .banner_info_desc {
+      font-size: 14px;
+    }
+  }
+`;
