546375329535181c8a169d491c273c5cc43dead0
kimHyeonJu <kimhyesom1202@gmail.com>
2024-04-28 16:36:56 +0900

    💄 styled-components 설치 및 적용

--- 1_rock-paper-scissors/src/App.styled.js
+++ 1_rock-paper-scissors/src/App.styled.js
@@ -1,18 +1,6 @@
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  font-family: "Madimi One", sans-serif;
-  font-weight: 400;
-  font-style: normal;
-}
-button {
-  background-color: inherit;
-  cursor: pointer;
-  border: 0;
-}
+import styled from 'styled-components';
 
-.wrap {
+export const Container = styled.div`
   position: relative;
   display: flex;
   flex-direction: column;
@@ -24,18 +12,17 @@ button {
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center center;
-
+  
   @media screen and (max-width: 960px) {}
-  @media screen and (max-width: 600px) {
 
-  }
-}
+  @media screen and (max-width: 600px) {}
+`;
 
-.box-wrap {
+export const BoxWrap = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  margin: 30px 0;
+  margin: 30px;
 
   @media screen and (max-width: 960px) {
     margin: 60px 0; 
@@ -44,64 +31,92 @@ button {
     flex-direction: column;
     margin: 40px 0;
   }
-}
 
-/* button */
-.button-group {
-  display: flex;
-}
-.button-group .button {
-  display: block;
-  width: 50px;
-  height: 50px;
-  margin-right: 20px;
+  .vs {
+    width: 300px;
+    height: 180px;
 
-  @media screen and (max-width: 960px) {
-    width: 60px;
-    height: 60px;
+    @media screen and (max-width: 960px) {
+      width: 200px;
+      height: 100px;
+    }
+    @media screen and (max-width: 600px) {
+      width: 170px;
+      height: 75px;
+      margin: 10px 0;
+    }
   }
-  @media screen and (max-width: 600px) {
+`;
+
+export const ButtonGroup = styled.div`
+  display: flex;
+
+  .button {
+    display: block;
     width: 50px;
     height: 50px;
-  }
-}
-.button-group .button:last-child {
-  margin-right: 0;
-}
-.button-group .button > img {
-  display: block;
-  width: 100%;
-  height: 100%;
-  transition: transform 300ms ease-in-out;
-}
-.button-group .button > img:hover {
-  transform: scale(1.2);
-}
+    margin-right: 20px;
 
-.vs {
-  width: 300px;
-  height: 180px;
+    &:last-child {
+      margin-right: 0;
+    }
 
-  @media screen and (max-width: 960px) {
-    width: 200px;
-    height: 100px;
-  }
-  @media screen and (max-width: 600px) {
-    width: 170px;
-    height: 75px;
-    margin: 10px 0;
-  }
-}
+    img {
+      display: block;
+      width: 100%;
+      height: 100%;
+      transition: transform 300ms ease-in-out;
 
-.count {
+      &:hover {
+        transform: scale(1.2);
+      }
+    }
+
+    @media screen and (max-width: 960px) {
+      width: 60px;
+      height: 60px;
+    }
+    @media screen and (max-width: 600px) {
+      width: 50px;
+      height: 50px;
+    }
+  }
+`;
+
+export const Count = styled.div`
   font-size: 30px;
+  font-weight: bold;
 
   @media screen and (max-width: 960px) {
     font-size: 40px;
   }
-}
+`;
 
-.reset {
+export const How = styled.button`
+  position: absolute;
+  top: 10%;
+  left: 10%;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background-color: #000;
+  font-weight: bold;
+  color: #fff;
+  transition: opacity 300ms ease-in-out;
+
+  &:hover {
+    opacity: 0.7;
+  }
+  
+  @media screen and (max-width: 960px) {
+    top: 20%;
+  }
+  @media screen and (max-width: 600px) {
+    top: 5%;
+  }
+`;
+
+export const Reset = styled.button`
   position: absolute;
   top: 10%;
   right: 10%;
@@ -118,32 +133,9 @@ button {
   @media screen and (max-width: 600px) {
     top: 5%;
   }
-}
+`;
 
-.how {
-  position: absolute;
-  top: 10%;
-  left: 10%;
-  width: 50px;
-  height: 50px;
-  border-radius: 50%;
-  background-color: #000;
-  font-weight: bold;
-  color: #fff;
-  transition: opacity 300ms ease-in-out;
-
-  @media screen and (max-width: 960px) {
-    top: 20%;
-  }
-  @media screen and (max-width: 600px) {
-    top: 5%;
-  }
-}
-.reset:hover, .how:hover {
-  opacity: 0.7;
-}
-
-.overlay {
+export const Overlay = styled.div`
   position: absolute;
   top: 0;
   left: 0;
@@ -151,4 +143,4 @@ button {
   height: 100vh;
   background-color: rgba(0, 0, 0, 0.7);
   z-index: 1;
-}
+`;
